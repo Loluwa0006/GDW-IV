e99273da923dc64f9055e90d7d0bc8ae9cb90bdf
Eman Awo <emanawosi4@gmail.com>
2025-12-31 15:47:46 -0500

    Run Trails
    Added black lines to show movement for running

--- Assets/Scripts/Characters/Deflector/States/RunState.cs
+++ Assets/Scripts/Characters/Deflector/States/RunState.cs
@@ -1,5 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RunState : SpeakerMoveState
 {
+
+    [SerializeField] ParticleSystem runTrails;
+
+    public override void InitState(BaseCharacter cha, CharacterStateMachine s_machine)
+    {
+        base.InitState(cha, s_machine);
+        if (runTrails != null) runTrails.Stop();
+    }
+
+    public override void Enter(Dictionary<string, object> msg = null)
+    {
+        base.Enter(msg);
+        if (runTrails != null) runTrails.Play();
+    }
+
+    public override void Process()
+    {
+        base.Process();
+        runTrails.transform.rotation = Quaternion.LookRotation(-character.velocityManager.GetInternalSpeed());
+    }
+    public override void Exit()
+    {
+        base.Exit();
+        if (runTrails != null) runTrails.Stop();
+    }
+
 }
