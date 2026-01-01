e07a2be0996e410d01b729353e3d928469a4e59e
Eman Awo <emanawosi4@gmail.com>
2025-12-31 20:38:18 -0500

    Terrain Bounce Particle Effects
    Added white circle explosion when echo collides with terrain

--- Assets/Scripts/Characters/Ball/TerrainBounceState.cs
+++ Assets/Scripts/Characters/Ball/TerrainBounceState.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 
 public class TerrainBounceState : BounceState
 {
+
+    [SerializeField] ParticleSystem bounceParticles;
     public override void Enter(Dictionary<string, object> msg = null)
     {
        if (echo.playerControlled)
         {
             echo.staminaComponent.EnableForesight();
         }
+        if (bounceParticles != null) bounceParticles.Play();
         base.Enter(msg);
     }
  
